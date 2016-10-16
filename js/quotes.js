f317b9a17837b4453b5028f7f6f50ffbe2d35c67
J. Reel <thereeljess@gmail.com>
2016-10-16 11:49:41 -0400

    theming a responsive layout

--- js/quotes.js
+++ js/quotes.js
@@ -1,5 +1,6 @@
 var quoteList = [
     ["Argue with idiots, and you become an idiot.", "Paul Graham"],
     ["Be yourself; everyone else is already taken.", "Oscar Wilde"],
-    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"]
+    ["Simplicity is the ultimate sophistication.", "Leonardo Da Vinci"],
+    ["The pure and simple truth is rarely pure and never simple.","Oscar Wilde"]
 ];
